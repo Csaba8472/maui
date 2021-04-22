4ecadbf3e7ba660581db31005f1e8d4ecb2f0b36
Shane Neuville <shneuvil@microsoft.com>
2021-04-22 14:18:53 -0500

    - fix

--- src/Essentials/test/DeviceTests.Shared/Utils.cs
+++ src/Essentials/test/DeviceTests.Shared/Utils.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Maui.Essentials.DeviceTests
 	{
 		public static void Unused(params object[] obj) { }
 
-#if WINDOWS
+#if WINDOWS_UWP
         public static async Task OnMainThread(Windows.UI.Core.DispatchedHandler action)
         {
             var mainView = Windows.ApplicationModel.Core.CoreApplication.MainView;
